144a634e0a7b1860137e547c95c44e6e3d21bed2
atayeem <tayeemmachine@gmail.com>
2026-01-31 23:07:47 -0500

    error on line 245

--- src/fft.c
+++ src/fft.c
@@ -1,19 +1,42 @@
-#pragma once
-
 #include <assert.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
 #include <fftw3.h>
 #include <string.h>
 #include <sndfile.h>
 
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+
+static float* generate_hann_window(size_t sz) {
+    float* w = calloc(sz, sizeof(float));
+    assert(w);
+
+    for (size_t n = 0; n < sz; n++) {
+        w[n] = 0.5 * (1 - cos(2.0 * M_PI * n / (sz - 1)));
+    }
+    return w;
+}
+
+static float* generate_none_window(size_t sz) {
+    float* w = calloc(sz, sizeof(float));
+    assert(w);
+
+    for (size_t n = 0; n < sz; n++) {
+        w[n] = 1.0;
+    }
+    return w;
+}
+
 enum WindowFunction {
     WF_HANN,
+    WF_NONE,
 };
 
 typedef struct {
-    enum WindowFunction window_function;
+    float* window_function;
 
     size_t window_size;
     size_t hop_size;
@@ -33,6 +56,12 @@ typedef struct {
     int channels;
 } Audiodata;
 
+// Remember, this stores them as a contiguous array of Audiodata, not an array of pointers to Audiodata.
+typedef struct {
+    int count;
+    Audiodata* data;
+} AudiodataMany;
+
 typedef struct {
     // Justification: in a reversal operation, the default behavior (non-specified) should be to preserve sample rate.
     size_t sample_rate;
@@ -42,15 +71,51 @@ typedef struct {
 
     size_t window_count;
 
-    // Guaranteed to have a size of sd->window_count * fk->window_size.
+    // Guaranteed to have a size of sd->window_count * (fk->window_size / 2 + 1).
     // Yes, it is necessarily associated with the kernel.
     fftwf_complex* data;
 
     // There is no option for interlacing windows. Just seems like unnecessary copying.
 } Spectrodata;
 
-Audiodata* audiodata_read_file(const char* fname)
-{
+AudiodataMany* audiodata_split_channels(const Audiodata* ad) {
+    AudiodataMany* am = calloc(1, sizeof(AudiodataMany));
+    assert(am);
+
+    int channel_count = ad->channels;
+    am->count = channel_count;
+    am->data = calloc(channel_count, sizeof(Audiodata));
+    assert(am->data);
+
+    for (int i = 0; i < channel_count; i++) {
+        Audiodata new_ad = {
+            .channels = 1,
+            .data = calloc(ad->frames, sizeof(float)),
+            .frames = ad->frames,
+            .sample_rate = ad->sample_rate
+        };
+        assert(new_ad.data);
+        am->data[i] = new_ad;
+    }
+
+    for (size_t frame = 0; frame < ad->frames; frame++) {
+        for (int channel = 0; channel < channel_count; channel++) {
+            am->data[channel].data[frame] = ad->data[channel_count * frame + channel];
+        }
+    }
+
+    return am;
+}
+
+void audiodata_many_destroy(AudiodataMany *am) {
+    for (int i = 0; i < am->count; i++)
+        free(am->data[i].data);
+    free(am->data);
+    free(am);
+}
+
+// Check return value.
+Audiodata* audiodata_read_file(const char* fname) {
     SF_INFO sfinfo = {};
 
     SNDFILE *sndfile = sf_open(fname, SFM_READ, &sfinfo);
@@ -75,16 +140,48 @@ Audiodata* audiodata_read_file(const char* fname)
     return ret;
 }
 
+void audiodata_write_file(const char* fname, const Audiodata* ad) {
+    SF_INFO sfinfo = {
+        .channels = ad->channels,
+        .format = SF_FORMAT_WAV | SF_FORMAT_PCM_16,
+        .frames = ad->frames,
+        .samplerate = ad->sample_rate,
+    };
+
+    SNDFILE *sndfile = sf_open(fname, SFM_WRITE, &sfinfo);
+    if (!sndfile) {
+        fprintf(stderr, "Failed to open audio file for writing '%s': %s\n", fname, sf_strerror(NULL));
+        return;
+    }
+
+    sf_count_t written = sf_writef_float(sndfile, ad->data, ad->frames);
+    if ((size_t)written < ad->frames) {
+        fprintf(stderr, "Couldn't write all frames (%lld/%lld) to audio file '%s': %s\n", written, ad->frames, fname, sf_strerror(sndfile));
+    }
+    fprintf(stderr, "Yeah. %s\n", sf_strerror(sndfile));
+
+    sf_close(sndfile);
+}
+
 void audiodata_destroy(Audiodata* ad) {
     free(ad->data);
     free(ad);
 }
 
+// Must succeed.
 FFTKernel* fftkernel_create(enum WindowFunction window_function, size_t window_size, size_t hop_size) {
     FFTKernel *ret = calloc(1, sizeof(FFTKernel));
     assert(ret);
 
-    ret->window_function = window_function;
+    switch(window_function) {
+        case WF_NONE:
+            ret->window_function = generate_none_window(window_size);
+        break;
+
+        case WF_HANN:
+            ret->window_function = generate_hann_window(window_size);
+        break;
+    }
 
     ret->window_size = window_size;
     ret->hop_size = hop_size;
@@ -106,16 +203,17 @@ void fftkernel_destroy(FFTKernel* fk) {
     fftwf_destroy_plan(fk->reverse);
     fftwf_free(fk->time_buf);
     fftwf_free(fk->freq_buf);
+    free(fk->window_function);
     free(fk);
 }
 
-Spectrodata* fftkernel_execute_forward(const FFTKernel* fk, const Audiodata *ad) {
+Spectrodata* fftkernel_execute_forward(const FFTKernel* fk, const Audiodata* ad) {
     if (ad->channels != 1) {
-        fprintf(stderr, "fftkernel_execute_forward: An Audiodata that wasn't one channel was given. It had %d channels.", ad->channels);
+        fprintf(stderr, "fftkernel_execute_forward: An Audiodata that wasn't one channel was given. It had %d channels.\n", ad->channels);
         return NULL;
     }
 
-    Spectrodata* sd = calloc(1, sizeof(Spectrodata));
+    Spectrodata *const sd = calloc(1, sizeof(Spectrodata));
     assert(sd);
 
     sd->sample_rate = ad->sample_rate;
@@ -126,9 +224,9 @@ Spectrodata* fftkernel_execute_forward(const FFTKernel* fk, const Audiodata *ad)
     assert(sd->data);
 
     fftwf_complex *sptr = sd->data;
-    const float* aptr_end = ad->data + ad->frames;
+    const float* const aptr_end = ad->data + ad->frames;
 
-    for (float* aptr = ad->data; aptr < aptr_end; aptr += fk->hop_size) {
+    for (const float* aptr = ad->data; aptr < aptr_end; aptr += fk->hop_size) {
         
         if (aptr + fk->hop_size > aptr_end) {
             memset(fk->time_buf, 0, fk->window_size);
@@ -137,15 +235,79 @@ Spectrodata* fftkernel_execute_forward(const FFTKernel* fk, const Audiodata *ad)
             memcpy(fk->time_buf, aptr, fk->window_size * sizeof(float));
         }
 
+        // Hanning or whatever else
+        for (size_t i = 0; i < fk->window_size; i++) {
+            fk->time_buf[i] *= fk->window_function[i];
+        }
+
         fftwf_execute(fk->forward);
 
         memcpy(sptr, fk->freq_buf, (fk->window_size / 2 + 1) * sizeof(fftwf_complex));
 
         sptr += (fk->window_size / 2 + 1);
     }
+
+    return sd;
+}
+
+Audiodata* fftkernel_execute_reverse(const FFTKernel* fk, const Spectrodata* sd) {
+    Audiodata *const ad = calloc(1, sizeof(Audiodata));
+    assert(ad);
+    ad->data = calloc(sd->original_length, sizeof(float));
+    assert(ad->data);
+    ad->channels = 1;
+    ad->frames = sd->original_length;
+    ad->sample_rate = sd->sample_rate;
+    
+    const size_t spec_size = fk->window_size / 2 + 1;
+    const fftwf_complex *const sptr_end = sd->data + sd->window_count * spec_size;
+
+    float* aptr = ad->data;
+    const float* const aptr_end = ad->data + ad->frames;
+    for (const fftwf_complex* sptr = sd->data; sptr < sptr_end; sptr += spec_size) {
+        // No secondary check required because it's going to be a perfect multiple of the block size for spectrodata.
+
+        memcpy(fk->freq_buf, sptr, spec_size);
+        
+        fftwf_execute(fk->reverse);
+
+        // OLA algorithm
+        for (size_t i = 0; i < MIN(fk->window_size, (size_t)(aptr_end - aptr)); i++)
+            aptr[i] += fk->time_buf[i];
+        
+        aptr += fk->hop_size;
+    }
+
+    return ad;
 }
 
 void spectrodata_destroy(Spectrodata *sd) {
     free(sd->data);
     free(sd);
+}
+
+int main() {
+    printf("Hello world!");
+    FFTKernel *fk = fftkernel_create(WF_HANN, 4096, 2048);
+
+    Audiodata *ad = audiodata_read_file("bin\\test.wav");
+    assert(ad);
+
+    AudiodataMany *am = audiodata_split_channels(ad);
+
+    Spectrodata *sd = fftkernel_execute_forward(fk, &am->data[0]);
+    assert(sd);
+
+    Audiodata *ad2 = fftkernel_execute_reverse(fk, sd);
+    assert(ad2);
+
+    printf("Got here.\n");
+    audiodata_write_file("bin\\test_out.wav", ad2);
+
+    audiodata_destroy(ad);
+    audiodata_many_destroy(am);
+    fftkernel_destroy(fk);
+    spectrodata_destroy(sd);
+
+    return 0;
 }
